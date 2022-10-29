a6be6dbf984502774c372b63f06f6d383b446a06
Marika <marika.meinander@outlook.com>
2022-10-29 11:56:18 +0300

    Add query page with request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
@@ -6,16 +6,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</<h1>'
+    return '<h1>Hello World</h1>'
+
 
 @app.route('/home', methods=['POST', 'GET'], defaults={'name': 'Default'})
 @app.route('/home/<string:name>', methods=['POST', 'GET'])
 def home(name):
-    return f'<h1>Hello {name}, you are on the home page</<h1>'
+    return f'<h1>Hello {name}, you are on the home page</h1>'
+
 
 @app.route('/json')
 def json():
     return jsonify({'key': 'value', 'key2': [1, 2, 3]})
+
+
+@app.route('/query')
+def query():
+    # example:
+    # /query?name=sara&location=Florida
+    name = request.args.get('name')
+    location = request.args.get('location')
+    return f'<h1>Hi {name}. You are from {location}. You are on the query page! </h1>'
+
 
 # one way to run flask app:
 if __name__ == '__main__':
